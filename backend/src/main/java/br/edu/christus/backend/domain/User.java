d6f48a140eda6d59c81daa4d0a217779ee5585a3
Adriano Candido <analista.adrianolima@gmail.com>
2026-09-12 11:31:33 -0300

    aula 11-09

---
+++ backend/src/main/java/br/edu/christus/backend/domain/User.java
@@ -0,0 +1,27 @@
+package br.edu.christus.backend.domain;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+
+@Getter
+@Setter
+@NoArgsConstructor
+@Entity
+@Table(name = "tb_user")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(length = 100, nullable = false)
+    private String name;
+
+    @Column(unique = true, nullable = false)
+    private String email;
+    private String password;
+
+
+}
---
+++ backend/src/main/java/br/edu/christus/backend/repository/UserRepository.java
@@ -0,0 +1,8 @@
+package br.edu.christus.backend.repository;
+
+import br.edu.christus.backend.domain.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository
+        extends JpaRepository<User, Long> {
+}
